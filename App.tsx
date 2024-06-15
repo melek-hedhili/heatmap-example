08562d04b3cdf9a5133826b5e32b61bc2e45a6de
melek <melek@github.com>
2024-06-15 15:18:00 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,205 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState, useRef } from "react";
+import {
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Platform,
+  ScrollView,
+} from "react-native";
+import MapView, { Polygon, Heatmap } from "react-native-maps";
+import {
+  generateHeatmapPoints,
+  initialRegion,
+  mapDarkStyle,
+  mapStandardStyle,
+} from "./utils";
+import { allPolygonCoordinates } from "./data";
+import { MaterialIcons } from "@expo/vector-icons";
+const CHIP_WIDTH = 120;
+const SCROLL_PADDING = 20;
 export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const mapRef = useRef<MapView>(null);
+  const scrollViewRef = useRef<ScrollView>(null);
+  const [selectedPolygonIndex, setSelectedPolygonIndex] = useState(null);
+
+  const handlePolygonPress = (index: any) => {
+    setSelectedPolygonIndex(index);
+
+    // Zoom into the selected polygon
+    if (mapRef.current && index !== null) {
+      const coordinates = allPolygonCoordinates[index];
+      mapRef.current.animateToRegion({
+        ...initialRegion,
+        latitude: coordinates[0].latitude,
+        longitude: coordinates[0].longitude,
+        //zoom into the polygon
+        latitudeDelta: 0.002,
+        longitudeDelta: 0.002,
+      });
+      // Scroll to the selected polygon chip
+      setTimeout(() => {
+        if (scrollViewRef.current) {
+          scrollViewRef.current.scrollTo({
+            x: index * CHIP_WIDTH - SCROLL_PADDING,
+            animated: true,
+          });
+        }
+      }, 200);
+    }
+  };
+
+  const renderPolygons = () =>
+    allPolygonCoordinates.map((polygonCoordinates, index) => {
+      const heatmapPoints = generateHeatmapPoints(polygonCoordinates);
+      return (
+        <View key={index}>
+          <Polygon
+            coordinates={polygonCoordinates}
+            strokeColor="#55d"
+            strokeWidth={5}
+            tappable={true}
+            onPress={() => handlePolygonPress(index)}
+          />
+          <Heatmap points={heatmapPoints} radius={10} opacity={1} />
+        </View>
+      );
+    });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        onPress={() => {
+          setSelectedPolygonIndex(null);
+          //zoom out to the initial region and set the scroll to the start
+
+          if (mapRef.current && scrollViewRef.current) {
+            mapRef.current.animateToRegion(initialRegion);
+            scrollViewRef.current.scrollTo({ x: 0, animated: true });
+          }
+        }}
+        ref={mapRef}
+        style={styles.map}
+        initialRegion={initialRegion}
+        customMapStyle={isDarkMode ? mapDarkStyle : mapStandardStyle}
+      >
+        {renderPolygons()}
+      </MapView>
+
+      <ScrollView
+        ref={scrollViewRef}
+        horizontal
+        scrollEventThrottle={1}
+        showsHorizontalScrollIndicator={false}
+        style={styles.chipsScrollView}
+        contentInset={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 20,
+        }}
+        contentContainerStyle={{
+          paddingRight: Platform.OS === "android" ? 20 : 0,
+        }}
+      >
+        {allPolygonCoordinates.map((_, index) => (
+          <TouchableOpacity
+            style={[
+              styles.chipsItem,
+              {
+                backgroundColor:
+                  //adapt to dark and light mode
+                  isDarkMode
+                    ? selectedPolygonIndex === index
+                      ? "#55d"
+                      : "#333"
+                    : selectedPolygonIndex === index
+                    ? "#55d"
+                    : "#fff",
+              },
+            ]}
+            onPress={() => handlePolygonPress(index)}
+          >
+            <Text
+              style={{
+                color: isDarkMode
+                  ? selectedPolygonIndex === index
+                    ? "#fff"
+                    : "#55d"
+                  : selectedPolygonIndex === index
+                  ? "#fff"
+                  : "#333",
+              }}
+            >{`Heatmap ${index + 1}`}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+
+      <TouchableOpacity
+        style={[
+          styles.header,
+          {
+            backgroundColor: isDarkMode ? "#333" : "#55d",
+          },
+        ]}
+        onPress={() => setIsDarkMode((prev) => !prev)}
+      >
+        <MaterialIcons
+          name={isDarkMode ? "wb-sunny" : "brightness-2"}
+          size={28}
+          color="white"
+        />
+      </TouchableOpacity>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
+  },
+  header: {
+    position: "absolute",
+    top: 50,
+    right: 5,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  carousel: {
+    height: 50,
+    width: "100%",
+  },
+  flatList: {
+    backgroundColor: "red",
+  },
+  chipsScrollView: {
+    position: "absolute",
+    bottom: 60,
+    paddingHorizontal: 10,
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 15,
+    marginRight: 10,
+    width: 80,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  chipsItem: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    padding: 8,
+    paddingHorizontal: 20,
+    marginHorizontal: 10,
+    height: 35,
   },
 });
---
+++ utils/index.ts
@@ -0,0 +1,390 @@
+import { LatLng } from "react-native-maps";
+const initialRegion = {
+  latitude: 35.82996805747307,
+  longitude: 10.63431853428483,
+  latitudeDelta: 0.009042349039951603,
+  longitudeDelta: 0.005305074155330658,
+};
+// Generate heatmap points inside the polygon
+const generateHeatmapPoints = (
+  polygon: LatLng[],
+  density: number = 20
+): { latitude: number; longitude: number; weight: number }[] => {
+  const bounds = polygon.reduce(
+    (bounds, coord) => {
+      bounds.minLat = Math.min(bounds.minLat, coord.latitude);
+      bounds.maxLat = Math.max(bounds.maxLat, coord.latitude);
+      bounds.minLng = Math.min(bounds.minLng, coord.longitude);
+      bounds.maxLng = Math.max(bounds.maxLng, coord.longitude);
+      return bounds;
+    },
+    {
+      minLat: Infinity,
+      maxLat: -Infinity,
+      minLng: Infinity,
+      maxLng: -Infinity,
+    }
+  );
+
+  const heatmapPoints: {
+    latitude: number;
+    longitude: number;
+    weight: number;
+  }[] = [];
+  const latStep = (bounds.maxLat - bounds.minLat) / density;
+  const lngStep = (bounds.maxLng - bounds.minLng) / density;
+
+  for (let lat = bounds.minLat; lat <= bounds.maxLat; lat += latStep) {
+    for (let lng = bounds.minLng; lng <= bounds.maxLng; lng += lngStep) {
+      const point = { latitude: lat, longitude: lng };
+      if (isPointInPolygon(point, polygon)) {
+        heatmapPoints.push({ ...point, weight: 5 });
+      }
+    }
+  }
+  return heatmapPoints;
+};
+// Utility function to check if a point is inside the polygon
+const isPointInPolygon = (point: LatLng, polygon: LatLng[]): boolean => {
+  let x = point.latitude,
+    y = point.longitude;
+  let inside = false;
+  for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+    let xi = polygon[i].latitude,
+      yi = polygon[i].longitude;
+    let xj = polygon[j].latitude,
+      yj = polygon[j].longitude;
+    let intersect =
+      yi > y !== yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+    if (intersect) inside = !inside;
+  }
+  return inside;
+};
+const mapDarkStyle = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#242f3e",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#746855",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#242f3e",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#d59563",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#d59563",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#263c3f",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#6b9a76",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#38414e",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#212a37",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#9ca5b3",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#746855",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#1f2835",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#f3d19c",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#2f3948",
+      },
+    ],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#d59563",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#17263c",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#515c6d",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#17263c",
+      },
+    ],
+  },
+];
+const mapStandardStyle = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#f5f5f5",
+      },
+    ],
+  },
+  {
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#616161",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#f5f5f5",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#bdbdbd",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#eeeeee",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#757575",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#e5e5e5",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#9e9e9e",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#ffffff",
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#757575",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#dadada",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#616161",
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#9e9e9e",
+      },
+    ],
+  },
+  {
+    featureType: "transit.line",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#e5e5e5",
+      },
+    ],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#eeeeee",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#c9c9c9",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#9e9e9e",
+      },
+    ],
+  },
+];
+export {
+  generateHeatmapPoints,
+  isPointInPolygon,
+  initialRegion,
+  mapDarkStyle,
+  mapStandardStyle,
+};
